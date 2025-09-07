5fc6339fc0e0266c793b69d1a397ef924c0d8041
anwar <dev.anwarul@gmail.com>
2025-09-07 23:45:38 +0600

    javascript practice

---
+++ assets/js/custom.js
@@ -0,0 +1,140 @@
+
+
+// let output = 10;
+
+// output -= 2;
+
+
+// console.log(output);
+
+// let age = 18;
+
+// if(age == 18) {
+//     console.log("tumi boro hoye geco!!");
+// }
+
+// else {
+//     console.log("tumi ekhono soto!!")
+// }
+
+
+let check = 200;
+
+// if(check === "10") {
+//     console.log('milce');
+// } 
+
+// if(check !== "10") {
+//     console.log('milce');
+// } 
+
+// if(check <= 10) {
+//     console.log('milce');
+// } 
+
+
+// if(check > 20) {
+//     console.log('20 theke boro');
+// }
+
+// else if(check > 10) {
+//     console.log('10 theke boro');
+// }
+
+// else {
+//     console.log('mileni');
+// }
+
+// check > 20 ? console.log('20 theke boro') : console.log('mileni');
+
+// console.log("hello" + " " + "world");
+
+// let one = "hello";
+
+// one += " world";
+
+// console.log(one);
+
+
+// let studentName = "khaled";
+
+// let studentAge = 20;
+
+// if(studentName == "khaled" || studentAge == 18) {
+//     console.log('this is actual student');
+// }
+
+
+// let test1 = 10;
+
+// if(!test1 == 10) {
+//     console.log('milce');
+// }
+
+// Truthy
+// 1. string 
+// 2. integer 
+// 3. array
+// 4. object 
+// 5. true
+
+// falsy 
+// 1. false
+// 2. null 
+// 3. undefined
+// 4. ""
+// 5. 0
+
+
+// if(!"abcd") {
+//     console.log('ami sottobadi');
+// }
+
+
+function test() {
+    let a = 10;
+    let output = "";
+
+    if(a = 10) {
+        output = "this is 10";
+    } 
+    else {
+        output = "this is empty value";
+    }
+
+    console.log(output);
+}
+
+
+// test();
+
+// Index number;
+
+let studentNames = ["khaled", "jayed", "rashed"];
+let myInfo = [
+    "Khaled",
+    25,
+    "Dhaka",
+    "01987878787",
+    "myemail@gmail.com",
+    "Bangladesh",
+    "Islam"
+];
+
+// key, value
+
+let myInfoObj = {
+    name: "khaled",
+    age: 25,
+    address: "Dhaka",
+    phoneNumber: "01728619733",
+    email: "nasrullah.cit.bd@gmail.com",
+    country: "Bangladesh",
+    religion: "Islam"
+}
+
+console.log(myInfo);
+console.log(myInfoObj);
+
+
+
