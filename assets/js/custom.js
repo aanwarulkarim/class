248128c6b1f029ba27893706deafc03c8e9bfa88
anwar <dev.anwarul@gmail.com>
2025-09-07 23:32:56 +0600

    class 23

---
+++ assets/js/custom.js
@@ -0,0 +1,43 @@
+
+
+
+// document.write ("VVgsywsy");
+// console.log('this is log');
+
+// window.alert("Hi kemon aso");
+
+// prompt('Tomar boyos koto?');
+
+
+// var x = 10;
+
+//  let x = 10;
+//  x = 20; 
+
+
+// const x = 10;
+
+
+// var test = 50;
+
+// var test = 100;
+
+ 
+
+// let test = 10;
+
+// test = 20;
+
+// const test = 10;
+// test = 20;
+
+// console.log(test);
+
+// const webName = website.com 
+
+
+let a = 3;
+
+
+
+
